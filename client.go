1d978b2324cff1418ab1c26d2fdb08ca67d48a81
brajeshk <brajeshk.119@gmail.com>
2015-05-22 15:00:33 -0700

    Client traffic pattern and counter

--- client.go
+++ client.go
@@ -6,29 +6,71 @@ import (
 	"io/ioutil"
 	"runtime"
 	"time"
+        "strconv"
+        "os"
 )
 
-func temp() {
-	resp, err := http.Get("http://localhost:8000/server2/")
+func temp(server string, count *int) {
+	resp, err := http.Get("http://localhost:8000/" + server + "/")
 		if err!=nil{
 			fmt.Println("not working")	
 		}else{
 			body, _ := ioutil.ReadAll(resp.Body)
 			fmt.Printf("resp is :%s",body)
-
+                        *count = *count + 1
 		}
 }
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU() + 5)
-	for i:=0;i<100;i++{
-		go temp()
-	}
-	time.Sleep(100*time.Second)
-	fmt.Println("Complete");
-	for{
+        fmt.Println(os.Args)
+        cmd_type := os.Args[1]
+        number, _ := strconv.Atoi(os.Args[2])
+        req_sent_server1 := 0
+        req_sent_server2 := 0
+        reply_server1 := 0
+        reply_server2 := 0
 
+        reply_recv_server1 := make([]int, number)
+        reply_recv_server2 := make([]int, number)
+
+        switch cmd_type {
+            case "1":
+                //burst 
+                runtime.GOMAXPROCS(runtime.NumCPU() + 5)
+                for i:=0;i<number/2;i++{
+                        go temp("server1", &reply_recv_server1[i])
+                        req_sent_server1++
+                        go temp("server2", &reply_recv_server2[i])
+                        req_sent_server2++
+                }
+            case "2": 
+                //to server1
+                runtime.GOMAXPROCS(runtime.NumCPU() + 5)
+                for i:=0;i<number;i++{
+                        go temp("server1", &reply_recv_server1[i])
+                        req_sent_server1++
+                }
+            case "3": 
+                //to server2
+                runtime.GOMAXPROCS(runtime.NumCPU() + 5)
+                for i:=0;i<number;i++{
+                        go temp("server2", &reply_recv_server2[i])
+                        req_sent_server2++
+                }
+            default: 
+                fmt.Println("Not implemented")
+        }
+
+        time.Sleep(100*time.Second)
+	for i:=0; i<number; i++{
+            if reply_recv_server1[i] != 0{
+                reply_server1++
+            }
+            if reply_recv_server2[i] != 0{
+                reply_server2++
+            }
 	}
+	fmt.Println("Complete");
 }
 
 
